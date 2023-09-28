d6d2ef6a2d6e1f5e44dcf5c2c0a47a1257166ccf
Todor Todorov <todorcnc@gmail.com>
2023-09-28 09:21:26 +0200

    Created a consumer file for the websocket connections allowing creating connection sending and receiving messages

---
+++ server/api/consumers.py
@@ -0,0 +1,27 @@
+# This file is related to websockets
+import json
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class WebsocketConsumer(AsyncWebsocketConsumer):
+   
+    async def connect(self):
+        print("Trying to connect")
+        await self.accept()
+        await self.channel_layer.group_add("notifications", self.channel_name)
+    
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard("notifications", self.channel_name)
+    
+    async def send_notification(self, event):
+
+        await self.send(text_data=json.dumps(event))
+
+    # This function will handle messages received from the WebSocket
+    async def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json.get('content', None)
+        if message:
+            print(f"Received message: {message}")
+        else:
+            print("Received a message, but no content field found.")
