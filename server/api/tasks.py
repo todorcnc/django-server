2e7a77f2b14d24c4baf5f70344dabe57206464f8
Todor Todorov <todorcnc@gmail.com>
2023-09-28 09:21:54 +0200

    Created tasks file containing the celery tasks

--- server/api/tasks.py
+++ server/api/tasks.py
@@ -1,7 +1,19 @@
-from __future__ import absolute_import, unicode_literals
-
+# This file is related to celery
 from celery import shared_task
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 @shared_task
 def add(x, y):
+    for i in range(100):
+        # Send Progress 
+        # This how you end messages over the websocket with django channels
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            "notifications",
+            {
+                "type": "send_notification",
+                "message": f"{i}"
+            }
+        )
     return x + y
