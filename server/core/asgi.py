a843f8d2e36636623d0cc9b19f43b01a3194f746
Todor Todorov <todorcnc@gmail.com>
2023-09-28 09:22:58 +0200

    Updated the asgi sevrer so it can receive http and websocket connections

--- server/core/asgi.py
+++ server/core/asgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter
+from . import urls  # noqa isort:skip
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": URLRouter(urls.websocket_urlpatterns)
+    }
+)
