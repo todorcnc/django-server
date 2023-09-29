2a0897ab1a1ad9dc74cc5ac6964203eb66891e8a
Todor Todorov <todorcnc@gmail.com>
2023-09-29 09:41:18 +0200

    Running docker with nvidia image with cuda enabled for sdxl

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/bash
 
 # Collect all static files
 python manage.py collectstatic --no-input
@@ -9,6 +9,6 @@ python manage.py migrate
 
 # Start the server
 # python manage.py runserver 0.0.0.0:8000
-
+nvidia-smi
 # Start Server with Uvicorn to allow http and websocket connections
 uvicorn core.asgi:application --host 0.0.0.0 --port 8000 --workers 4 --log-level debug --reload
